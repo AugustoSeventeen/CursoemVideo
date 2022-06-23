febcb0e7f4959a6a870b4bf23182b0a4521f9d2c
AugustoSeventeen <augustovinicius806@gmail.com>
2022-06-23 11:30:02 -0300

    Initial Commit

--- ex078.py
+++ ex078.py
@@ -1,7 +1,22 @@
-# Reading Vowels
-words = ('Gate', 'School', 'Bus', 'Tree', 'Time', 'Portuguese', 'Battery', 'Drones', 'Mug', 'Criticized', 'Wall')
-for word in words:
-    print(f'\nIn word \033[36m{word}\033[m we have ', end=' ')
-    for letter in word:
-        if letter.lower() in 'aeiou':
-            print(f'\033[36m{letter}\033[m', end=' ')
+# Read 4 value and save in a list, tell what is the highest and lowest, and position
+numbers = []
+lar = 0
+low = 0
+for question in (range(0, 5)):
+    numbers.append(int(input('Enter a number: ')))
+    if question == 0:
+        lar = low = numbers[question]
+    if numbers[question] > lar:
+        lar = numbers[question]
+    if numbers[question] < low:
+        low = numbers[question]
+print(f'The highest value is \033[32m{lar}\033[m and it is in', end=' ')
+for position, item in enumerate(numbers):
+    if item == lar:
+        print(position, end=', ')
+        print('positions', end=', ')
+print(f'\nThe lowest value is \033[32m{low}\033[m and it is in ', end=' ')
+for position, item in enumerate(numbers):
+    if item == low:
+        print(position, end=', ')
+        print('positions', end=' ')
--- ex079.py
+++
@@ -1,9 +0,0 @@
-# Read 4 value and save in a list, tell what is the highest and lowest, and position
-numbers = []
-for question in range(0, 5):
-    questions = int(input('Enter a number: '))
-    numbers.append(questions)
-print(f'The highest value is \033[32m{max(numbers)}\033[m and it is in \033[34m{numbers.index(max(numbers)) + 1}ª\033[m'
-      f' position')
-print(f'The lowest value is \033[32m{min(numbers)}\033[m and it is in \033[34m{numbers.index(min(numbers)) + 1}ª\033[m'
-      f' position')
